334d214bcef8366508cac3b46ccae1b21515a142
liuzihan200545 <3069522719@qq.com>
2025-02-06 21:11:18 +0800

    Build spdlog logging system.

---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "MoonlessEngine"
+    architecture "x64"
+
+    configurations{
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputs = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MoonlessEngine"
+    location "MoonlessEngine"
+    kind "SharedLib"
+    language "C++"
+    
+    targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+    objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "%{wks.location}/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines{
+            "ML_PLATFORM_WINDOWS",
+            "ML_BUILD_DLL"
+        }
+
+        postbuildcommands{
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "ML_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ML_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "ML_DIST"
+        symbols "On"
+
+project "SandBox"
+    location "SandBox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" ..outputs.. "/%{prj.name}")
+    objdir ("bin-int/" ..outputs.. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs {
+        "%{wks.location}/include",
+        "MoonlessEngine/src"
+    }
+
+    filter "system:windows"
+    cppdialect "C++20"
+    staticruntime "On"
+    systemversion "latest"
+
+    defines{
+        "ML_PLATFORM_WINDOWS"
+    }
+
+    links{
+        "MoonlessEngine"
+    }
+
+    filter "configurations:Debug"
+        defines "ML_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ML_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "ML_DIST"
+        symbols "On"
