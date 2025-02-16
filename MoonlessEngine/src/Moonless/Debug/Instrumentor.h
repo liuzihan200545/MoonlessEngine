40094924e9c223abca048024c5989b341a4d6e94
liuzihan200545 <3069522719@qq.com>
2025-02-16 16:27:43 +0800

    Instrumentation fixes(thread safe)

--- MoonlessEngine/src/Moonless/Debug/Instrumentor.h
+++ MoonlessEngine/src/Moonless/Debug/Instrumentor.h
@@ -4,76 +4,116 @@
 #include <algorithm>
 #include <fstream>
 #include <thread>
+#include "Moonless/Core/Log.h"
 
 namespace Moonless {
 	struct ProfileResult
 	{
 		std::string Name;
 		long long Start, End;
-		uint32_t ThreadID;
+		std::thread::id ThreadID;
 	};
+
 	struct InstrumentationSession
 	{
 		std::string Name;
 	};
+
 	class Instrumentor
 	{
 	private:
+		std::mutex m_Mutex;
 		InstrumentationSession* m_CurrentSession;
 		std::ofstream m_OutputStream;
-		int m_ProfileCount;
 	public:
 		Instrumentor()
-			: m_CurrentSession(nullptr), m_ProfileCount(0)
+			: m_CurrentSession(nullptr)
 		{
 		}
+
 		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
 		{
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession) {
+				// If there is already a current session, then close it before beginning new one.
+				// Subsequent profiling output meant for the original session will end up in the
+				// newly opened session instead.  That's better than having badly formatted
+				// profiling output.
+				ML_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+
+				InternalEndSession();
+			}
 			m_OutputStream.open(filepath);
-			WriteHeader();
-			m_CurrentSession = new InstrumentationSession{ name };
+			if (m_OutputStream.is_open()) {
+				m_CurrentSession = new InstrumentationSession({name});
+				WriteHeader();
+			} else {
+				ML_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+			}
 		}
+
 		void EndSession()
 		{
-			WriteFooter();
-			m_OutputStream.close();
-			delete m_CurrentSession;
-			m_CurrentSession = nullptr;
-			m_ProfileCount = 0;
+			std::lock_guard lock(m_Mutex);
+			InternalEndSession();
 		}
+
 		void WriteProfile(const ProfileResult& result)
 		{
-			if (m_ProfileCount++ > 0)
-				m_OutputStream << ",";
+			std::stringstream json;
+
 			std::string name = result.Name;
 			std::replace(name.begin(), name.end(), '"', '\'');
-			m_OutputStream << "{";
-			m_OutputStream << "\"cat\":\"function\",";
-			m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-			m_OutputStream << "\"name\":\"" << name << "\",";
-			m_OutputStream << "\"ph\":\"X\",";
-			m_OutputStream << "\"pid\":0,";
-			m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-			m_OutputStream << "\"ts\":" << result.Start;
-			m_OutputStream << "}";
-			m_OutputStream.flush();
+
+			json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.End - result.Start) << ',';
+			json << "\"name\":\"" << name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.ThreadID << ",";
+			json << "\"ts\":" << result.Start;
+			json << "}";
+
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession) {
+				m_OutputStream << json.str();
+				m_OutputStream.flush();
+			}
 		}
+
+		static Instrumentor& Get() {
+			static Instrumentor instance;
+			return instance;
+		}
+
+	private:
+
 		void WriteHeader()
 		{
-			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
 			m_OutputStream.flush();
 		}
+
 		void WriteFooter()
 		{
 			m_OutputStream << "]}";
 			m_OutputStream.flush();
 		}
-		static Instrumentor& Get()
-		{
-			static Instrumentor instance;
-			return instance;
+
+		// Note: you must already own lock on m_Mutex before
+		// calling InternalEndSession()
+		void InternalEndSession() {
+			if (m_CurrentSession) {
+				WriteFooter();
+				m_OutputStream.close();
+				delete m_CurrentSession;
+				m_CurrentSession = nullptr;
+			}
 		}
+
 	};
+
 	class InstrumentationTimer
 	{
 	public:
@@ -82,18 +122,22 @@ namespace Moonless {
 		{
 			m_StartTimepoint = std::chrono::high_resolution_clock::now();
 		}
+
 		~InstrumentationTimer()
 		{
 			if (!m_Stopped)
 				Stop();
 		}
+
 		void Stop()
 		{
 			auto endTimepoint = std::chrono::high_resolution_clock::now();
+
 			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
 			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
-			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-			Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+
+			Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id()});
+
 			m_Stopped = true;
 		}
 	private:
